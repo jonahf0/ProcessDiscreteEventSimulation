ca5529a53a429e75b86f2879bc4b7999d5794432
Jonah Fitzgerald <jonahfitzgerald0113@gmail.com>
2026-05-03 19:56:52 -0400

    added logic to start parsing the strace logging into a collection of events mapped to unique processes

--- analyze_strace.py
+++ analyze_strace.py
@@ -1,4 +1,30 @@
 from argparse import ArgumentParser
+from regex import search,findall
+
+def create_dictionary(file):
+    results = {}
+
+    for line in file:
+        key = tuple(line.replace("<", " ").replace(">"," ").split()[0:2])
+        
+        if (not (key in results)):
+            results[key] = []
+        
+        try:
+            results[key].append( findall("[0-9]{2}:[0-9]{2}:[0-9]{2}|[a-zA-Z0-9_]+\\(",line) )
+        except Exception as e:
+            print(e)
+
+    return results
+
+
+def main(filepath):
+    with open(filepath) as f:
+        file = [ line.strip() for line in f ]
+    
+    results = create_dictionary(file)
+
+    print(results)
 
 if __name__ == "__main__":
     parser = ArgumentParser(usage="python3 analyze_strace.py <path to strace output>", description='Used to convert output from running "strace -t -Y -yy -f -p <target process PID> -o <output>" into a dataset')
