0f63c4d269b80cf36188942010ddd664e263a8ef
Jonah Fitzgerald <jonahfitzgerald0113@gmail.com>
2026-05-17 22:18:02 -0400

    reworked analyze_trace.py to also account for child processes; added a better overarching plan in to_do.txt

--- analyze_strace.py
+++ analyze_strace.py
@@ -1,44 +1,95 @@
 from argparse import ArgumentParser
 from regex import search,findall
 from pprint import pprint
+from pathlib import Path
+from itertools import chain
 
-def create_dictionary(file):
-    results = {}
 
-    for line in file:
-        key = tuple(line.replace("<", " ").replace(">"," ").split()[0:2])
+#structure for creating processes and tracking their children
+class ProcTree:
+    def __init__(self, name, pid):
+        self.pid = pid
+        self.name = name
+        self.children = []
+        self.call_list = []
         
-        if (not (key in results)):
-            results[key] = []
-        
-        try:
 
-            results[key].append( 
-                [
-                 item.strip("(") for item in findall("[0-9]{2}:[0-9]{2}:[0-9]{2}|[a-zA-Z0-9_]+\\(",line)[0:2] if len(item)
-                 ]
-            )
-        except Exception as e:
-            print(e)
+    def create_child(self, name, pid):
+        self.children.append( (proc:=ProcTree(name, pid)) )
+        return proc
 
-
-    for key in results.keys():
-        results[key] = [ item for item in results[key] if len(item) > 1 ]
-
-    return results
-
-
-def main(filepath):
-    with open(filepath) as f:
-        file = [ line.strip() for line in f ]
+    def add_call(self, call):
+        self.call_list.append(call)
     
-    results = create_dictionary(file)
+    def __str__(self):
+        #return f"{self.pid}|{self.name} -- {', '.join(list(map(lambda a: f'{a.pid}|{a.name}', self.children)))}"
+        return f"{self.pid}<{self.name}>: {', '.join([ f'{proc.pid}<{proc.name}>' for proc in self.children ])}" 
 
-    print([ result[1] for result in results[list(results.keys())[0]]])
+#takes the raw file split into lines
+def create_process_tree(file):
+    
+    #using a map to avoid duplicating a massive file into mem
+    split_lines = map(lambda line: line.split(" "), file)
+    
+    all_procs = dict()
+    root = None
+    while ( (line:=next(split_lines, None)) != None ):
+        proc_sig, time, call = line[0:3]
+        pid, name = proc_sig.replace(">", "").replace("<", " ").split(" ")
+
+        sig = (pid,name)
+        if not ( sig in all_procs ):
+            all_procs[sig] = ProcTree(name, pid)
+            root=sig if len(all_procs)==1 else root
+
+        current_proc = all_procs[sig]
+            
+        #use a match in case there's other weird calls to handle
+        match (call_name:=call.split("(")[0]):
+            case "clone":
+                #second to last item for a line that says "clone" should look like PID<PROC>;
+                #if not, try assuming the original
+                child_info = line[-2].strip()
+                c_pid, c_name = child_info.replace(">","").replace("<", " ").split(" ")
+                child = current_proc.create_child(c_name, c_pid)
+                all_procs[(c_pid,c_name)] = child
+
+            case "execve":
+                new_name = call.split('("')[1].split("/")[-1].strip('",')
+ 
+                current_proc = all_procs.pop(sig)
+                all_procs[(sig[0], new_name)] = current_proc
+                current_proc.name = new_name
+        
+        
+        current_proc.add_call( (call_name, time, line[-1].replace("<","").replace(">","").strip() ) )
+    
+    return root,all_procs
+
+def main(filepath,root):
+    
+    files = [ str(file) for file in Path(filepath).iterdir() ]
+    
+    #this locates the trace file for the root process first; this helps to insert it at the beginning of the list for processing
+    root_file = [ file for file in files if root in file ][0]
+    files.remove(root_file)
+    
+    combined_file = [ line for line in open(root_file) ]
+
+    combined_file = chain.from_iterable( [combined_file] + [ [ line.strip() for line in open(f) ] for f in files ] ) 
+    
+    root_sig, results = create_process_tree(combined_file)
+
+    pprint( [ str(results[proc]) for proc in results ] )
+
+    for proc in results:
+        print(results[proc].call_list)
+
 
 if __name__ == "__main__":
-    parser = ArgumentParser(usage="python3 analyze_strace.py <path to strace output>", description='Used to convert output from running "strace -t -Y -yy -f -p <target process PID> -o <output>" into a dataset')
+    parser = ArgumentParser(usage="python3 analyze_strace.py <path to strace output> <root file>", description='Used to convert output from running "strace -t -T -Y -yy --always-show-pid -ff -p <target process PID> -o <output>" into a dataset')
     parser.add_argument("filepath", help="This is the target strace output file")
+    parser.add_argument("root", help="This is the file for the root process (i.e., the one that you targeted with the -p flag in strace")
     args = parser.parse_args()
-
-    main(args.filepath)
+    
+    main(args.filepath, args.root)
